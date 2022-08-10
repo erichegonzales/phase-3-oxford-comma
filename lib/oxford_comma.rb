817f35082cd3b16a94370adf352a96b273df773f
Eriche Gonzales <erichelouise@gmail.com>
2022-08-10 14:13:41 -0400

    commit tree d8bf802fad797f368dcb32adbc6f2fe93bb09b3e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,27 @@
-def oxford_comma(array)
+require "pry"
 
+# def oxford_comma(array)
+#   if array.length == 1
+#     array.join
+#   elsif array.length == 2
+#     array.join(" and ")
+#   else
+#     array[0...array.length - 1].join(", ") << ", and #{array[-1]}"
+#   end
+# end
+
+# def oxford_comma(array)
+#   if array.length < 3
+#     array.join(" and ")
+#   else
+#     array[0...array.length - 1].join(", ") << ", and #{array[-1]}"
+#   end
+# end
+
+def oxford_comma(array)
+  return array.join(" and ") if array.length < 3
+
+  array[-1] = "and #{array[-1]}"
+
+  array.join(", ")
 end
